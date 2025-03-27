dfbe8e3668040847a4ff52d76e5fc5c0fab7527e
Tanishq Doke <112191079+TanishqDoke@users.noreply.github.com>
2025-03-27 09:06:39 +0000

    Stack and Heap Memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -10,7 +10,7 @@
 const id= Symbol("123")
 const anotherId=Symbol("123")
 
-console.log(id===anotherId);
+//console.log(id===anotherId);
 
 
 // Reference (Non Primitive)
@@ -20,13 +20,45 @@ console.log(id===anotherId);
 const heroes=["oggy","tom","jerry"];
 
 let myObj={
+    
+
+    // --------------------------------------------------------------------------
+
+
     name:"Tanishq",
     age:22
 }
 
 const muFunction= function(){
-    console.log("Hello World");
+    //console.log("Hello World");
     
 }
 
 // typeof(null) => object 
+
+
+// --------------------------------------------------------------------------
+
+// Stack(Primitive)  Heap(Non-Primitive)
+
+let youtubename="Tanishq Doke"
+
+let anothername=youtubename  // Get reference as copy
+anothername="Doke Tanishq"
+
+// console.log(youtubename);
+// console.log(anothername);
+
+
+let userOne={
+    email:"tdoke@mail.com",
+    upi: "tdoke@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.upi="thd@ybl"
+
+console.log(userTwo.upi);
+console.log(userOne.upi);
+
