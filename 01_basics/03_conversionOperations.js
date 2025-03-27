788b91a6e7ceeae1e57f569d30f8ad991f6e5f6e
Tanishq Doke <112191079+TanishqDoke@users.noreply.github.com>
2025-03-27 08:34:31 +0000

    Conversions in js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,30 +1,41 @@
-// let score ="33"
+let score ="33"
 
 // console.log(typeof score)
 
 
-// let valueInNumber=Number(score)
+let valueInNumber=Number(score)
 // console.log(typeof valueInNumber)
 
-// let b="24t"
-// let bnum = Number(b)
+let b="24t"
+let bnum = Number(b)
 // console.log(bnum)   // Output is Nan
 
-// let c=null
-// let cnum=Number(c)
+let c=null
+let cnum=Number(c)
 // console.log(cnum)   // Output is 0
 
-// let d   // undefined
-// let dnum=Number(d)
+let d   // undefined
+let dnum=Number(d)
 // console.log(dnum)   // Output is Nan
 
 let isLoggedin=1
 
 let booleanLoggedIn=Boolean(isLoggedin)
-console.log(booleanLoggedIn)
+// console.log(booleanLoggedIn)
 
 // 1 -> true
 // 0 -> false
 // "tanishq" -> true
 // "" -> false
+
+
+// ************************************ Operations ************************************************
+
+console.log("1"+2);
+console.log(1 +"2");
+
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
 
