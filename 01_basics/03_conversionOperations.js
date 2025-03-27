4add2c147504e55c39c882aeedd29c108946bc8f
Tanishq Doke <112191079+TanishqDoke@users.noreply.github.com>
2025-03-27 07:14:37 +0000

    coversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+// let score ="33"
+
+// console.log(typeof score)
+
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+
+// let b="24t"
+// let bnum = Number(b)
+// console.log(bnum)   // Output is Nan
+
+// let c=null
+// let cnum=Number(c)
+// console.log(cnum)   // Output is 0
+
+// let d   // undefined
+// let dnum=Number(d)
+// console.log(dnum)   // Output is Nan
+
+let isLoggedin=1
+
+let booleanLoggedIn=Boolean(isLoggedin)
+console.log(booleanLoggedIn)
+
+// 1 -> true
+// 0 -> false
+// "tanishq" -> true
+// "" -> false
+
