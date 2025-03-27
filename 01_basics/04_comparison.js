59e235bfbad3d294caef89bfa9fae0fe4c9dc805
Tanishq Doke <112191079+TanishqDoke@users.noreply.github.com>
2025-03-27 08:34:47 +0000

    Comparison Operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log("2">0)
+// console.log("02">0)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// stict check ===
+
+console.log("2"==2);
+console.log("2"===2);
+
