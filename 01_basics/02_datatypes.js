8445f7e66ea9adc8efa480de7300450da5b24e5f
Tanishq Doke <112191079+TanishqDoke@users.noreply.github.com>
2025-03-27 07:01:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treated as newer version of js
+
+// alert(3+3) It will not work since we are using node js, not browser
+
+let name="Tanishq"  //String
+let age=22          //Number
+let isLoggedin=true // Number
+let address         // Undefined
+let a=null          //null value
+null // standalone value
+
+//symbol => for uniqueness
+
+
+console.log(typeof null)        // object
+console.log(typeof undefined)   // undefined
